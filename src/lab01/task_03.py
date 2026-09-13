24a102a2926d5058850e395bee66fe12a0c2b429
MSankov <sankov2008@gmail.com>
2026-09-14 00:15:36 +0300

    fic_output

--- src/lab01/task_03.py
+++ src/lab01/task_03.py
@@ -10,6 +10,6 @@ total = base + vat_amount
 #vat_amount = f'vat_amount = {vat_amount:.2f}'
 #total = f'total = {total:.2f}'
 
-print(f'{"База после скидки:":<20} {base:.2f} ₽')
-print(f'{"НДС:":<20} {vat_amount:.2f}  ₽')
-print(f'{"Итого к оплате:":<20} {total:.2f} ₽')
+print(f'{"База после скидки:":>20} {base:.2f} ₽')
+print(f'{"НДС:":>20} {vat_amount:.2f}  ₽')
+print(f'{"Итого к оплате:":>20} {total:.2f} ₽')
